7324b20064b564bcec47eb0c0c7bcbc25ebc1007
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-25 16:09:13 +0200

    aggiunta css per square differenti

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// costante del contenitore
 const squareContainer = document.getElementById("containerSquare");
 
+// ciclo base per la ripetizione degli square
 for (let i = 1; i < 1000; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
