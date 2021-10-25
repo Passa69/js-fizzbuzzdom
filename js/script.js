4663e2b612fcdc174be855c37bba319977bb7f4f
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-25 15:50:55 +0200

    ciclo semplice

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const squareContainer = document.getElementById("containerSquare");
 
 for (let i = 1; i < 1000; i++) {
-    
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.append(i);
+    squareContainer.append(square);
 }
