f949896d97214f25e088291ed83b7e2a0f4e0aa0
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-25 16:29:57 +0200

    aggiunta condizioni per il ciclo

--- js/script.js
+++ js/script.js
@@ -7,4 +7,17 @@ for (let i = 1; i < 1000; i++) {
     square.classList.add("square");
     square.append(i);
     squareContainer.append(square);
+
+    if (i % 3 == 0) {
+        square.classList.add("square3");
+        square.append("fizz");
+
+    } else if (i % 5 == 0) {
+        square.classList.add("square5");
+        square.append("buzz");
+
+    } else (i % 15 == 0); {
+        square.classList.add("square15");
+        square.append("fizzbuzz");
+    }
 }
